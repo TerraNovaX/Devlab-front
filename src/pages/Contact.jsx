f14da65ea68d14f29bda89508e05ccdf6ee09fa1
selimo11 <s.oueslati.contact@gmail.com>
2025-01-31 14:53:38 +0100

    Front (feat): Contact page and its components added + tailwind.config.js edited

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import ContactForm from "../components/ContactForm.jsx";
 
 const DropOffDevice = () => {
-    return <div>Contact</div>;
+    return <div>
+        <ContactForm/>
+    </div>;
 };
 
 export default DropOffDevice;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
-    fontFamily: {
-      title: ["Fjalla One", "sans-serif"],
-      texte: ["Roboto", "sans-serif"],
-    },
-    colors: {
-      darkBlue: "#004D94",
-      skyBlue: "#06CDFC",
-      green: "#A6C356",
-      yellow: "#FFD539",
-      black: "#000000",
-      white: "#FFFFFF",
-    },
   },
   plugins: [],
-};
+}
