df480764ee0290d8162654abff5d04cc8b91faa9
Hamza_Idb <hamza.idblhy@gmail.com>
2025-01-31 16:35:17 +0100

    header

--- src/pages/DropOffDevice.jsx
+++ src/pages/DropOffDevice.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import NavigationMenu from "../components/NavigationMenu.jsx";
+import Header from "../components/Header.jsx";
+import React, { useState, useEffect } from "react";
 
 const DropOffDevice = () => {
-  return <div>DropOffDevice</div>;
+
+  const [headerData, setHeaderData] = useState({
+          backgroundImage: '/src/assets/formation_img.png',
+          title: '',
+          breadcrumb: []
+      });
+  
+      useEffect(() => {
+          // Fetch or set the data specific to the Formation page
+          setHeaderData({
+              backgroundImage: '/src/assets/formation_img.png',
+              title: 'Déposer',
+              breadcrumb: [
+                  { href: '/', label: 'Home' },
+                  { href: '/DropOffDevice', label: 'Déposer' }
+              ]
+          });
+      }, []);
+
+  return <div>
+    <NavigationMenu/>
+    <Header 
+          backgroundImage={headerData.backgroundImage} 
+          title={headerData.title} 
+          breadcrumb={headerData.breadcrumb} 
+      />
+    DropOffDevice</div>;
 };
 
 export default DropOffDevice;
